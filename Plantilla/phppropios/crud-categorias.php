6f8d43ff5b7cba0ba49d9f359ff3e0005943a95a
Alejandro Tamayo <31549601+AlejandroTC@users.noreply.github.com>
2022-05-17 20:46:12 -0500

    Add files via upload

--- Plantilla/phppropios/crud-categorias.php
+++ Plantilla/phppropios/crud-categorias.php
@@ -1,41 +1,28 @@
 <?php
-    
-    //include 'conexion.php';
+    //GET
+    //POST
+    //Respuesta al cliente (Chrome, Edge, Firefox, cualquier naveegador)JSON
+    //include: 'conexion.php';
     $conexion = 1;
-
     $Respuesta = array();
-    $accion    = $_POST['accion'];
-
+    $accion = $_POST["accion"];
     switch ($accion) {
         case 'create':
-            actionCreatePHP($conexion);
+            actionCreatePHP();
             break;
         case 'update':
             actionUpdatePHP();
             break;
+        
         case 'delete':
             actionDeletePHP();
             break;
+        
         case 'read':
             actionReadPHP();
             break;
-        default:
-            # code...
-            break;
     }
-
-    function actionReadPHP(){
-        $Respuesta['estado']=1;
-        $Respuesta['mensaje']="Los registro se listan correctamente";
-        echo json_encode($Respuesta);    
-    }
-
-    function actionDeletePHP(){
-        $Respuesta['estado']=1;
-        $Respuesta['mensaje']="El registro se elimino correctamente";
-        echo json_encode($Respuesta);
-    }
-
+    
     function actionCreatePHP($conexion){
         $categoria    = $_POST['categoria'];
         $subcategoria = $_POST['subcategorias'];
@@ -60,10 +47,34 @@
         //Generar la consulta para agregar
         echo json_encode($Respuesta);
     }
-    
+
     function actionUpdatePHP(){
-        $Respuesta['estado']=1;
-        $Respuesta['mensaje']="El registro se actualizo correctamente";
+        $Respuesta['estado'] = 1;
+        $Respuesta['mensaje'] = "El registro se actualizo correctamente";
         echo json_encode($Respuesta);
     }
+
+    function actionDeletePHP(){
+        $categoria = $_POST['categoria'];
+        $subcategoria = $_POST['subcategoria'];
+        //Consulta para crear una Categoria o Rubro
+        $QueryCreate = "INSERT INTO categoria(id, nombrecategoria, subcategoria) VALUES (NULL,'".$categoria"',".$subcategoria")";
+        if(mysqli_query($conexion, $QueryCreate)){
+            $Respuesta['estado'] = 1;
+            $Respuesta['mensaje'] = "El registro se guardo correctamente";
+            $respuesta['id'] = mysqli_insert_id($conexion);
+        }
+        else {
+            $Respuesta['estado'] = 0;
+            $Respuesta['mensaje'] = "Ocurrio un error desconocido";
+        }
+        echo json_encode($Respuesta);
+    }
+
+    function actionReadPHP(){
+        $Respuesta['estado'] = 1;
+        $Respuesta['mensaje'] = "Los registros se listan correctamente";
+        echo json_encode($Respuesta);
+    }
+    //echo "Hola Mundo de las categorias o rubros usando AJAX: ".$_POST['categoria'];
 ?>
